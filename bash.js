042ec41899d8e3e0e8693dcef8c027281841a64a
Robby0501 <robbyulrich@hotmail.com>
2022-08-03 14:00:51 -0600

    update

--- bash.js
+++ bash.js
@@ -3,8 +3,11 @@ process.stdout.write('prompt >');
 
 //The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) =>{
-    const cmd =data.toString().trim(); // remove the newLine
+    const cmd = data.toString().trim(); // remove the newLine
 
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt >');
+    if (cmd ==="pwd") {
+        console.log(process.cwd());
+    }
 })
